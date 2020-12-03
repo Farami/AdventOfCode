c26a87807c78465121cdcfdeb0e0adfe89c71dfa
Niclas O'Connor <oconnor@plt-software.de>
2020-12-03 10:06:37 +0100

    Add 2020 Day 3

---
+++ 2020/Day 3/Part 1.fsx
@@ -0,0 +1,22 @@
+open System.IO
+
+let map =
+    File.ReadAllText("input.txt").Split '\n'
+    |> Seq.map Seq.toList
+    |> Seq.toList
+
+let traverse xStep yStep (map: List<List<char>>) =
+  let mapWidth = map.[0].Length
+  let mapHeight = map.Length
+
+  let getPoint x y = map.[y].[x % mapWidth]
+  let isTree = (=) '#'
+
+  let rec innerTraverse x y (sum: uint32) =
+      match y with
+      | y when y >= mapHeight - 1 -> sum
+      | _ -> innerTraverse (x + xStep) (y + yStep) (sum + if isTree <| getPoint x y then 1u else 0u)
+  innerTraverse 0 0 0u
+
+traverse 3 1 map
+|> printfn "%d"
