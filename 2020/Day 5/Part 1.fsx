2a92645a3386afa148ce6d4f0183ac30054351a9
Niclas O'Connor <oconnor@plt-software.de>
2020-12-08 08:24:02 +0100

    Add 2020 Day 5

---
+++ 2020/Day 5/Part 1.fsx
@@ -0,0 +1,33 @@
+open System.IO
+
+let input = File.ReadAllText("input.txt").Split('\n') |> Array.toList
+
+let partition (seats: int list) step = 
+  let partitionSize = seats.Length / 2
+  let partitions = List.splitAt partitionSize seats
+  match step with
+  | 'F' | 'L' -> fst partitions
+  | 'B' | 'R' -> snd partitions
+  | _ ->  []
+
+let rec parse (seats: int list) (stepSequence: char list) =
+  match stepSequence with
+  | head::tail -> parse (partition seats head) tail 
+  | [] -> seats.Head 
+
+let parseRow stepSequence =
+  stepSequence
+  |> List.take 7
+  |> parse [0..128]
+
+let parseColumn stepSequence =
+  stepSequence
+  |> List.skip 7
+  |> parse [0..7]
+
+let calculateId row column = row * 8 + column
+
+input
+|> List.map ((fun x -> x.ToCharArray() |> Array.toList) >> (fun x -> calculateId (parseRow x) (parseColumn x))) 
+|> List.max
+|> printfn "%d"
